d62cbad3fd1737bdaa49e0870a8e96a7a5e52325
Stephanehk <stephane@utexas.edu>
2020-12-18 23:38:27 -0500

    updated ppo network

--- examples/running_carla/run_ppo_navigation.py
+++ examples/running_carla/run_ppo_navigation.py
@@ -8,6 +8,11 @@ import math
 import random
 from sklearn.neighbors import KDTree
 import sys
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.distributions import MultivariateNormal
+import wandb
 
 from traffic_events import TrafficEventType
 from statistics_manager import StatisticManager
@@ -138,7 +143,9 @@ class CarEnv:
         #velocity_kmh = int(3.6*np.sqrt(np.power(velocity.x,2) + np.power(velocity.y,2) + np.power(velocity.z,2)))
         velocity_mag = np.sqrt(np.power(velocity.x,2) + np.power(velocity.y,2) + np.power(velocity.z,2))
         self.cur_velocity = velocity_mag
-        state = (self.rgb_cam,command_encoded,velocity_mag,d2target)
+
+        state = [self.rgb_cam,velocity_mag,d2target]
+        state.extend(command_encoded)
 
         #check for traffic light infraction/stoplight infraction
         #TODO: Right now stoplights and traffic lights are handled the same, which is incorrect
@@ -158,7 +165,7 @@ class CarEnv:
         else:
             done = False
             self.events.append([TrafficEventType.ROUTE_COMPLETION, self.d_completed])
-        
+
         #get reward information
         self.statistics_manager.compute_route_statistics(time.time(), self.events)
         reward = self.statistics_manager.route_record["score_composed"] - self.statistics_manager.prev_score
@@ -169,7 +176,7 @@ class CarEnv:
         if self.cur_velocity > 0 and self.blocked == True:
             self.blocked = False
             self.blocked_start = 0
-        return state, reward, done, None
+        return state, reward, done, [self.statistics_manager.route_record['score_route']]
 
     def cleanup(self):
         for actor in self.actors:
@@ -189,23 +196,207 @@ class CarEnv:
             self.route_commands.append(route.get(waypoint))
         self.route_kdtree = KDTree(np.array(self.route_waypoints))
 
-def run_navigation(host,world_port):
+#device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+class PPO_Agent(nn.Module):
+    def __init__(self, linear_state_dim, action_dim, action_std):
+        super(PPO_Agent, self).__init__()
+        # action mean range -1 to 1
+        self.actorConv = nn.Sequential(
+                nn.Conv2d(3,6,5),
+                nn.Tanh(),
+                nn.MaxPool2d(2,2),
+                nn.Conv2d(6,12,5),
+                nn.Tanh(),
+                nn.MaxPool2d(2,2),
+                nn.Flatten()
+                )
+        self.actorLin = nn.Sequential(
+                nn.Linear(12*17*17 + linear_state_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, action_dim),
+                nn.Tanh()
+                )
+
+        self.criticConv = nn.Sequential(
+                nn.Conv2d(3,6,5),
+                nn.Tanh(),
+                nn.MaxPool2d(2,2),
+                nn.Conv2d(6,12,5),
+                nn.Tanh(),
+                nn.MaxPool2d(2,2),
+                nn.Flatten()
+                )
+        self.criticLin = nn.Sequential(
+                nn.Linear(12*17*17 + linear_state_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, action_dim),
+                nn.Tanh()
+                )
+        self.action_var = torch.full((action_dim,), action_std*action_std).to(device)
+
+    def actor(self,state):
+        frame = state[0]
+        measurements = state[1]
+        measurements = measurements.unsqueeze(0)
+        vec = self.actorConv(frame)
+        X = torch.cat((vec,measurements),1)
+        return self.actorLin(X)
+
+    def critic(self,state):
+        frame = state[0]
+        measurements = state[1]
+        vec = self.criticConv(frame)
+        X = torch.cat((vec,measurements),0)
+        return self.criticLin(X)
+
+    def choose_action(self,state):
+        #state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        #state = torch.FloatTensor(state).to(device)
+        mean = self.actor(state)
+        cov_matrix = torch.diag(self.action_var)
+        gauss_dist = MultivariateNormal(mean,cov_matrix)
+        action = gauss_dist.sample()
+        action_log_prob = gauss_dist.log_prob(action)
+        return action, action_log_prob
+
+    def get_training_params(self, state, action):
+        #state = state.to(device)
+        #action = action.to(device)
+
+        state = torch.stack(state)
+        if len(list(state.size())) > 4:
+            state = torch.squeeze(state)
+        elif len(list(state.size())) == 3:
+            state = state.unsqueeze(0)
+        action = torch.stack(action)
+
+        mean = self.actor(state)
+        action_expanded = self.action_var.expand_as(mean)
+        cov_matrix = torch.diag_embed(action_expanded).to(device)
+
+        gauss_dist = MultivariateNormal(mean,cov_matrix)
+        action_log_prob = gauss_dist.log_prob(action).to(device)
+        entropy = gauss_dist.entropy().to(device)
+        state_value = torch.squeeze(self.critic(state)).to(device)
+        return action_log_prob, state_value, entropy
+
+def format_(state):
+    frame = torch.FloatTensor(state[0])
+    h,w,c = frame.shape
+    frame = frame.unsqueeze(0).view(1, c, h, w)
+
+    measurements = torch.FloatTensor(state[1:])
+    return [frame,measurements]
+
+def train_PPO(host,world_port):
+    wandb.init(project='PPO_Carla_Navigation')
+
     env = CarEnv(host,world_port)
-    done = False
-    s = env.reset()
-   
-    rewards = []
-    while not done:
-        a = [1,1] #go straight and slow
-        s_prime, reward, done, info = env.step(a)
-        rewards.append(reward)
+    n_iters = 1000
+    n_epochs = 50
+    max_steps = 2000
+    gamma = 0.9
+    lr = 0.0001
+    clip_val = 0.2
+    avg_t = 0
+    avg_r = 0
 
-    #print (len(env.events))
-    print (rewards)
-    #for e in env.events:
-    #    print (e)
-    env.cleanup()
+    config = wandb.config
+    config.learning_rate = lr
 
+
+    n_states = 8
+
+    #currently the action array will be [throttle, steer]
+    n_actions = 2
+
+    action_std = 0.5 #maybe try some other values for this
+    #init models
+    policy = PPO_Agent(n_states, n_actions, action_std).to(device)
+    policy.load_state_dict(torch.load("policy_state_dictionary.pt"))
+    optimizer = Adam(policy.parameters(), lr=lr)
+    mse = nn.MSELoss()
+
+    prev_policy = PPO_Agent(n_states, n_actions, action_std).to(device)
+    #TODO: idk if I should be setting each policies initial states to the same thing or not
+    prev_policy.load_state_dict(policy.state_dict())
+
+
+    wandb.watch(prev_policy)
+
+    for iters in range (n_iters):
+        s = env.reset()
+        t = 0
+        episode_reward = 0
+        done = False
+        rewards = []
+        states = []
+        actions = []
+        actions_log_probs = []
+        states_p = []
+        while not done:
+            #env.render()
+            a, a_log_prob = prev_policy.choose_action(format_(s).to(device))
+            s_prime, reward, done, info = env.step(a.detach())
+
+            states.append(format_(s))
+            actions.append(a)
+            actions_log_probs.append(a_log_prob)
+            rewards.append(reward)
+            states_p.append(format_(s_prime))
+
+            s = s_prime
+            t+=1
+            episode_reward+=reward
+
+        print ("Episode reward: " + str(episode_reward))
+        print ("Percept compleyed: " + str(info[0]))
+        avg_t+=t
+        avg_r+=episode_reward
+        env.cleanup()
+
+        #f = open("output.txt","a")
+        #f.write("ran episode with reward " + str(episode_reward))
+        #f.close()
+
+        wandb.log({"episode_reward": episode_reward})
+        wandb.log({"percent_completed": info[0]})
+        if (len(states) == 1):
+            continue
+
+        #format reward
+        for i in range (len(rewards)):
+            rewards[len(rewards)-1] = rewards[len(rewards)-1]*np.power(gamma,i)
+        rewards = torch.tensor(rewards).to(device)
+        rewards= (rewards-rewards.mean())/rewards.std()
+
+        actions_log_probs = torch.FloatTensor(actions_log_probs).to(device)
+        #train PPO
+        for i in range(n_epochs):
+            current_action_log_probs, state_values, entropies = policy.get_training_params(states,actions)
+
+            policy_ratio = torch.exp(current_action_log_probs - actions_log_probs.detach())
+            #policy_ratio = current_action_log_probs.detach()/actions_log_probs
+            advantage = rewards - state_values.detach()
+
+            update1 = (policy_ratio*advantage).float()
+            update2 = (torch.clamp(policy_ratio,1-clip_val, 1+clip_val) * advantage).float()
+            loss = -torch.min(update1,update2) + 0.5*mse(state_values.float(),rewards.float()) - 0.001*entropies
+
+            optimizer.zero_grad()
+            loss.mean().backward()
+            optimizer.step()
+            print ("    on epoch " + str(i))
+            #wandb.log({"loss": loss.mean()})
+
+        if iters % 50 == 0:
+            prev_policy.load_state_dict(policy.state_dict())
+            torch.save(policy.state_dict(),"policy_state_dictionary.pt")
 
 
 def main(n_vehicles,host,world_port,tm_port):
